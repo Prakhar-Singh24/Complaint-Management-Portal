d75ca59f303f41ef3909233456c1a4efc52f15e7
Prakhar Singh <prakharpk2412@gmail.com>
2026-06-22 21:18:47 +0530

    Updated Project

---
+++ script/script.js
@@ -0,0 +1,23 @@
+document.getElementById("loginUserBtn")?.addEventListener("click", () => {
+    window.location.href = "login.html";
+});
+
+document.getElementById("registerUserBtn")?.addEventListener("click", () => {
+    window.location.href = "register.html";
+});
+
+document.getElementById("registerComplaintBtn")?.addEventListener("click", () => {
+    window.location.href = "complaint.html";
+});
+
+document.getElementById("trackComplaintBtn")?.addEventListener("click", () => {
+    window.location.href = "track.html";
+});
+
+document.getElementById("logout")?.addEventListener("click", () => {
+    window.location.href = "index.html";
+});
+
+document.getElementById("aboutContact")?.addEventListener("click", () => {
+    window.location.href = "contact.html";
+});
